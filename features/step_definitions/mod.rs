aeb26fdee90eaf8d092ad88c32f693ad5ead4dd5
Benjamin Hase <benjamin.hase@web.de>
2018-10-08 08:24:48 +0200

    rename step strings

--- features/step_definitions/mod.rs
+++ features/step_definitions/mod.rs
@@ -2,20 +2,23 @@
 use support;
 use raytracer_challenge::tuple;
 
-define_str!(GIVEN_TUPLE, r"a := tuple\((", FLOAT!(), "), (", FLOAT!(), "), (", FLOAT!(), "), (", FLOAT!(), r")\)");
+define_str!(GIVEN_TUPLE_STEP, r"a := tuple\((", FLOAT!(), "), (", FLOAT!(), "), (", FLOAT!(), "), (", FLOAT!(), r")\)");
 
-define_str!(GIVEN_POINT, r"p := point\((", FLOAT!(), "), (", FLOAT!(), "), (", FLOAT!(), r")\)");
+define_str!(GIVEN_POINT_STEP, r"p := point\((", FLOAT!(), "), (", FLOAT!(), "), (", FLOAT!(), r")\)");
 
-const GIVEN_TUPLE_STEP: &'static str = GIVEN_TUPLE!();
-const GIVEN_POINT_STEP: &'static str = GIVEN_POINT!();
+define_str!(THEN_TUPLE_ELEMENT_STEP, r"a\.([xyzw]) = (", FLOAT!(), ")");
+
+const GIVEN_TUPLE: &'static str = GIVEN_TUPLE_STEP!();
+const GIVEN_POINT: &'static str = GIVEN_POINT_STEP!();
+const THEN_TUPLE_ELEMENT: &'static str = THEN_TUPLE_ELEMENT_STEP!();
 
 // Any type that implements cucumber_rust::World + Default can be the world
 steps!(support::MyWorld => {
-    given regex GIVEN_TUPLE_STEP
+    given regex GIVEN_TUPLE
         (f64, f64, f64, f64) |world, x, y, z, w, step| {
             world.tuple = tuple::Tuple { x, y, z, w };
     };
-    then regex r"a\.([xyzw]) = ([+-]?\d+\.\d+)" (char, f64) |world, part, value, step| {
+    then regex THEN_TUPLE_ELEMENT (char, f64) |world, part, value, step| {
         let v = match part {
             'x' => world.tuple.x,
             'y' => world.tuple.y,
@@ -40,7 +43,7 @@ steps!(support::MyWorld => {
         assert_eq!(world.tuple.is_vector(), expected_result);
     };
 
-    given regex GIVEN_POINT_STEP
+    given regex GIVEN_POINT
         (f64, f64, f64) |world, x, y, z, step| {
     };
 
